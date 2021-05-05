cbca5be5cdf603dc1d26653881d52d8fcd685091
Michelle Bergquist <m@michellescripts.com>
2021-05-05 15:11:06 -0600

    Gradle outputs nice test summary

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,4 +44,75 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    systemProperty("junit.jupiter.execution.parallel.enabled", true)
+
+    testLogging {
+        // set options for log level LIFECYCLE
+        events(
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+        )
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+
+        // set options for log level DEBUG and INFO
+        debug {
+            events(
+                org.gradle.api.tasks.testing.logging.TestLogEvent.STARTED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
+            )
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        }
+        info.events = debug.events
+        info.exceptionFormat = debug.exceptionFormat
+    }
+
+    var failures = ""
+
+    addTestListener(object : TestListener {
+        override fun beforeSuite(suite: TestDescriptor) {}
+        override fun beforeTest(testDescriptor: TestDescriptor) {}
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
+            if (result.resultType === TestResult.ResultType.FAILURE) {
+                failures += " * ${testDescriptor.className} > \n\t${testDescriptor.name}\n"
+            }
+        }
+
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.parent === null) { // will match the outermost suite
+                val purple = "\u001B[35m"
+                val red = "\u001B[31m"
+                val reset = "\u001B[0m"
+                val output =
+                    "Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} successes, ${result.failedTestCount} failures, ${result.skippedTestCount} skipped)"
+                val divider = "\n**********************************************************************************\n"
+
+                println(divider)
+                if (failures === "") {
+                    print(purple)
+                    println("SUCCESS")
+                    print(reset)
+                }
+
+                if (failures !== "") {
+                    println(red)
+                    println("FAILURES:")
+                    println("\n")
+                    println(failures + reset)
+                }
+
+                println("RESULTS:")
+                println(output)
+                println(divider)
+            }
+        }
+    })
 }
